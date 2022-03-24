c734007f58a71f8291bdc47f994f4df48f27b152
Jagadesh <32300213+jagadeshsubash@users.noreply.github.com>
2022-03-24 22:47:29 +0530

    Update car.js

--- src/car.js
+++ src/car.js
@@ -1,1 +1,14 @@
-Test
+function Car() {
+  return <h2>I am a Car!</h2>;
+}
+
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+    </>
+  );
+}
+
+ReactDOM.render(<Garage />, document.getElementById('root'));
