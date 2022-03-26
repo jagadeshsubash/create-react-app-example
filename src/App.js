ca0802ebdbcf5f41e993bf85d8cc616c43233214
jagadeshsubash <subash.jagadesh@gmail.com>
2022-03-26 22:59:35 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import  Hello f   "./components/Hello";
+import  Hello from  "./components/Hello";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          This is my first react app
-        </p>
+        <Hello></Hello>
       </div>
     );
   }
