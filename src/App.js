098428f6091e52b48323f21bf1ea3609243945d4
jagadeshsubash <subash.jagadesh@gmail.com>
2022-04-08 19:08:53 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Hello from "./Hello.js" ;
 
-class App extends Component {
+function App()  {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome tto React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello></Hello>
+  
       </div>
     );
   }
-}
+
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,4 @@
+function Hello() {
+  return <h1>JAGS</h1>
+}
+export default Hello;
